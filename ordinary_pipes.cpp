cfdbd913b574de9db89d7ef50c5380ccfd5e75ad
Upanishadkachroo <upanishadkachroo@gmail.com>
2025-04-15 18:12:53 +0530

    working of pipe

---
+++ ordinary_pipes.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<unistd.h>//pipe() & fork()
+#include<string.h>
+
+#define BUFFER_SIZE 25
+#define READ_END 0
+#define WRITE_END 1
+
+int main(){
+    char write_msg[BUFFER_SIZE]="Greetings";
+    char read_msg[BUFFER_SIZE];
+
+    int fd[2];//store file descriptors from pipe
+
+    if(pipe(fd)==-1){//if pipe creation fails
+        cerr<<"Pipe failed"<<endl;
+        return 1;
+    }
+
+    pid_t pid=fork();
+    if(pid<0){
+        cerr<<"Fork Failed"<<endl;
+        return 1;
+    }
+
+    if(pid>0){//parent process
+        close(fd[READ_END]);//parent only writes
+
+        write(fd[WRITE_END], write_msg, strlen(write_msg)+1);//write in pipe
+
+        close(fd[WRITE_END]);//close the write end of the pipe
+    }
+    else{//child process
+        close(fd[WRITE_END]);//child read only
+
+        read(fd[READ_END], read_msg, BUFFER_SIZE);
+
+        cout<<"Read: "<<read_msg<<endl;
+
+        close(fd[READ_END]);
+    }
+    return 0;
+}
